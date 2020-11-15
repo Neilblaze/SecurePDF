1139e006c4c35b437d0f7866d8c630ac7c0fadf0
Pratyay Banerjee <48355572+Neilblaze@users.noreply.github.com>
2020-11-16 02:50:51 +0530

    updated app
    Included
    • Header
    • LayoutContainer

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
 import React from "react";
 import App, { Container } from "next/app";
 import Head from "next/head";
-
+import Header from "../components/header.js";
+import LayoutContainer from "../components/container.js";
 
 class Layout extends React.Component {
   render() {
@@ -13,8 +14,21 @@ class Layout extends React.Component {
             SecurePDF &mdash; Share with Care!
           </title>
         </Head>
+        <Header />
       </div>
     );
   }
 }
 
+export default class MyApp extends App {
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <Container>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </Container>
+    );
+  }
+}
