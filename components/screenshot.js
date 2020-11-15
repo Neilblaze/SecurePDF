a1df42b51562fb5b7ab7e3378598ddccb5ac1eb9
Pratyay Banerjee <48355572+Neilblaze@users.noreply.github.com>
2020-11-16 03:03:24 +0530

    screenshot_aftr hero section component

---
+++ components/screenshot.js
@@ -0,0 +1,32 @@
+import HideOnMobile from "./hide-on-mobile";
+
+export default function Screenshot() {
+  return (
+    <>
+      <style jsx>
+        {`
+          .screenshot {
+            position: relative;
+            text-align: center;
+            margin: 3rem 0 2rem 0;
+            background-size: cover;
+            background-repeat: no-repeat;
+            background-position: center center;
+            background-image: url(/static/screenshot.png);
+          }
+
+          .screenshot {
+            border-radius: 5px;
+            width: 100%;
+            box-shadow: 0 3px 16px rgba(0, 0, 0, 0.2);
+          }
+        `}
+      </style>
+      <div className="screenshot">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1786 1340"></svg>
+        <HideOnMobile>
+        </HideOnMobile>
+      </div>
+    </>
+  );
+}
