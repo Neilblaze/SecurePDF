8231730375a37a144dfb5b919e882b175d4ee6c1
Pratyay Banerjee <48355572+Neilblaze@users.noreply.github.com>
2020-11-16 02:53:54 +0530

    added button

---
+++ components/button.js
@@ -0,0 +1,51 @@
+import classNames from "classnames";
+
+export default function Button({
+  children,
+  onClick,
+  compact,
+  href,
+  className
+}) {
+  const classes = classNames(className, "button", {
+    compact
+  });
+
+  return (
+    <a className={classes} href={href} onClick={onClick}>
+      <style jsx>{`
+        .button {
+          font-size: 14px;
+          font-weight: 700;
+          text-transform: uppercase;
+          color: #fff;
+          height: 46px;
+          padding: 0 22px;
+          cursor: pointer;
+          text-decoration: none;
+          border-radius: calc(46px / 2);
+          line-height: 46px;
+          display: inline-block;
+          box-sizing: border-box;
+          background-color: #1c1f2b;
+        }
+
+        .button:hover {
+          text-decoration: none;
+        }
+
+        .button.compact {
+          line-height: 45px;
+          font-size: 13px;
+          height: 45px;
+          border-radius: calc(45px / 2);
+        }
+
+        .button:hover {
+          background: #30354a;
+        }
+      `}</style>
+      <span>{children}</span>
+    </a>
+  );
+}
