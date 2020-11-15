c7bf1810a667b786e0ce47152a3bb4b7b9dc39d0
Pratyay Banerjee <48355572+Neilblaze@users.noreply.github.com>
2020-11-16 03:01:19 +0530

    install_button component
    NOTE : Links not yet configured

---
+++ components/installButton.js
@@ -0,0 +1,31 @@
+import Button from "./button";
+import Bowser from "bowser";
+// Bowser is used to detect if device is on Android/Chrome (Deprecated ~ 15.11.20)
+
+export default function Install({ children, compact }) {
+  const supported = {
+    chrome: {
+      eventName: 'install: android',
+      text: "Download now for Android",
+      url:
+        "https://github.com/Neilblaze/SecurePDF/XYZ"
+    },
+  };
+
+  const browser = Bowser.getParser(window.navigator.userAgent);
+  const details = supported[browser.getBrowser().name.toLowerCase()] || supported.chrome;
+  const buttonUrl = details.url;
+  const buttonLabel = compact ? "Install" : details.text;
+
+  return (
+    <Button
+      compact={compact}
+      href={buttonUrl}
+      onClick={function() {
+        window.plausible(details.eventName)
+      }}
+    >
+      {buttonLabel}
+    </Button>
+  );
+}
