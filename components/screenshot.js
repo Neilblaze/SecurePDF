c9f769f4b79ca163b838f7169bafee95e3ae9e82
Pratyay Banerjee <48355572+Neilblaze@users.noreply.github.com>
2020-11-16 03:03:59 +0530

    added responsiveness & foot note

--- components/screenshot.js
+++ components/screenshot.js
@@ -20,6 +20,23 @@ export default function Screenshot() {
             width: 100%;
             box-shadow: 0 3px 16px rgba(0, 0, 0, 0.2);
           }
+
+          @media only screen and (min-width: 768px) {
+            .screenshot {
+              margin: 6rem auto 1rem auto;
+              background-image: url(/static/screenshot.png);
+            }
+          }
+
+          .live-demo {
+            text-align: center;
+          }
+
+          @media only screen and (min-width: 768px) {
+            .live-demo {
+              margin: 2rem 0 5rem 0;
+            }
+          }
         `}
       </style>
       <div className="screenshot">
@@ -27,6 +44,9 @@ export default function Screenshot() {
         <HideOnMobile>
         </HideOnMobile>
       </div>
+      <p className="live-demo">
+        Check out the <a href="https://youtube.com/">Demo</a>, on Youtube!
+      </p>
     </>
   );
 }
